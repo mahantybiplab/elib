81dd99b8975d9bfee35d1b63807ebd3513e7b3f5
biplab mahanty <bmahanty501@gmail.com>
2025-03-08 07:24:06 +0000

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -13,9 +13,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required!");
     return next(error);
   }
-  
-   // Database call.
-   try {
+
+  // Database call.
+  try {
     const user = await userModel.findOne({ email });
     if (user) {
       const error = createHttpError(
@@ -56,4 +56,37 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found."));
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(createHttpError(400, "Username or password incorrect!"));
+    }
+
+    // Proceed with login success logic here (e.g., generating a token)
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Internal Server Error"));
+  }
+
+  // todo: handle errors
+  // Create accesstoken
+};
+
+export { createUser, loginUser };
