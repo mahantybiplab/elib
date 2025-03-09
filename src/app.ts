9d08d0c9e5151e5802f3a81163aa666e93ec2904
biplab mahanty <bmahanty501@gmail.com>
2025-03-09 09:51:52 +0000

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import express from "express";
-
+import cors from "cors";
 import userRouter from "./user/userRouter";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the elib api" });
-  
 });
 
 app.use("/api/users", userRouter);
